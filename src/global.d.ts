23d6ec2906276633a4b2a5e01c9f39a66fdb673c
Frank Kulak <fs.kulak@gmail.com>
2022-05-18 23:13:17 -0400

    Create common types.

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,40 @@
 /// <reference types="svelte" />
+
+import type { StringTableLocale } from "@s4tk/models/enums";
+
+interface Stbl {
+  entries: StblEntry[];
+  locale: StringTableLocale;
+}
+
+interface StblEntry {
+  isUpdated?: boolean;
+  key: string;
+  value: string;
+}
+
+interface StblProject {
+  group: string;
+  id: string;
+  instance: string;
+  name: string;
+  primaryLocale: StringTableLocale;
+  stbls: Stbl[];
+
+  toJson(): string;
+}
+
+interface Workspace {
+  version: number;
+  projects: StblProject[];
+  settings: WorkspaceSettings;
+
+  toJson(): string;
+}
+
+interface WorkspaceSettings {
+  creatorName: string;
+  defaultLocale: StringTableLocale;
+  isDarkTheme: boolean;
+  nextProjectId: number;
+}
