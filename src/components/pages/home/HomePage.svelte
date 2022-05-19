7f6780d1ed771033cfb8bde07154a6d7f7397d8e
Frank Kulak <fs.kulak@gmail.com>
2022-05-19 09:24:53 -0400

    Add select mode toggle on home page.

--- src/components/pages/home/HomePage.svelte
+++ src/components/pages/home/HomePage.svelte
@@ -2,6 +2,65 @@
   import ToolbarColor from "../../../enums/toolbar-colors";
   import ContentArea from "../../shared/ContentArea.svelte";
   import FloatingActionButtons from "../../shared/FloatingActionButtons.svelte";
+
+  let selectMode = false;
+  $: selectModeToggleText = selectMode ? "X" : "select";
+
+  const normalModeToolbar = [
+    {
+      title: "save workspace",
+      icon: "desktop-download",
+      color: ToolbarColor.Download,
+      onClick() {
+        console.log("Save Clicked");
+      },
+    },
+    {
+      title: "upload",
+      icon: "upload",
+      color: ToolbarColor.Upload,
+      onClick() {
+        console.log("Upload Clicked");
+      },
+    },
+    {
+      title: "create",
+      icon: "plus",
+      color: ToolbarColor.Create,
+      onClick() {
+        console.log("Create Clicked");
+      },
+    },
+  ];
+
+  const selectModeToolbar = [
+    {
+      title: "delete",
+      icon: "trash",
+      color: ToolbarColor.Delete,
+      onClick() {
+        console.log("Delete Clicked");
+      },
+    },
+    {
+      title: "merge",
+      icon: "git-merge",
+      color: ToolbarColor.Merge,
+      onClick() {
+        console.log("Merge Clicked");
+      },
+    },
+    {
+      title: "download",
+      icon: "download",
+      color: ToolbarColor.Download,
+      onClick() {
+        console.log("Download Clicked");
+      },
+    },
+  ];
+
+  $: toolbarData = selectMode ? selectModeToolbar : normalModeToolbar;
 </script>
 
 <svelte:head>
@@ -12,54 +71,30 @@
     <slot>First</slot>
   </ContentArea>
   <ContentArea banded={true}>
-    <slot>Second</slot>
+    <slot>
+      <div>
+        <button
+          class="select-toggle"
+          on:click={() => (selectMode = !selectMode)}
+          >{selectModeToggleText}</button
+        >
+      </div>
+    </slot>
   </ContentArea>
 </section>
-<FloatingActionButtons
-  buttonData={[
-    {
-      title: "save workspace",
-      icon: "desktop-download",
-      color: ToolbarColor.Download,
-      onClick() {
-        console.log("Save Clicked");
-      },
-    },
-    {
-      title: "upload stbl",
-      icon: "upload",
-      color: ToolbarColor.Upload,
-      onClick() {
-        console.log("Upload Clicked");
-      },
-    },
-    {
-      title: "create stbl",
-      icon: "plus",
-      color: ToolbarColor.Create,
-      onClick() {
-        console.log("Create Clicked");
-      },
-    },
-    {
-      title: "merge stbls",
-      icon: "git-merge",
-      color: ToolbarColor.Merge,
-      onClick() {
-        console.log("Merge Clicked");
-      },
-    },
-    {
-      title: "delete stbls",
-      icon: "trash",
-      color: ToolbarColor.Delete,
-      onClick() {
-        console.log("Delete Clicked");
-      },
-    },
-  ]}
-/>
+<FloatingActionButtons buttonData={toolbarData} />
 
 <style lang="scss">
-  // intentionally blank
+  .select-toggle {
+    border: none;
+    background: none;
+    color: var(--color-text);
+    text-transform: uppercase;
+    opacity: 0.6;
+
+    &:hover {
+      opacity: 1;
+      cursor: pointer;
+    }
+  }
 </style>
