759436ab86bd2e9586360c38eb29eb1fd0c2424f
Frank Kulak <fs.kulak@gmail.com>
2022-05-21 00:56:17 -0400

    Update OnboardingView.svelte

--- src/components/views/OnboardingView.svelte
+++ src/components/views/OnboardingView.svelte
@@ -2,18 +2,25 @@
   import NavigationButton from "../shared/NavigationButton.svelte";
   import ProgressCircles from "../shared/ProgressCircles.svelte";
 
+  let page: "name" | "disclaimers" | "upload" = "name";
+
   let creatorName: string = "";
 
   $: nameFilledIn = Boolean(creatorName);
 
+  $: filledInCircles =
+    (nameFilledIn ? 1 : 0) + (page === "disclaimers" ? 1 : 0);
+
   function nextButtonClicked() {
-    // TODO:
-    alert("next");
+    if (page === "name") {
+      page = "disclaimers";
+    } else if (page === "disclaimers") {
+      // TODO:
+    }
   }
 
   function uploadWorkspaceButtonClicked() {
-    // TODO:
-    alert("upload");
+    page = "upload";
   }
 </script>
 
@@ -30,37 +37,56 @@
     </p>
   </div>
   <div class="mb-2">
-    <p>
-      String Table Studio is a web app that makes it easy to create, edit, and
-      translate string tables for Sims 4 mods. View the <a
-        href="https://github.com/sims4toolkit/stbl-studio-web/README.md"
-        target="_blank">feature list</a
-      > to learn more about what it can do.
-    </p>
-    <p class="mb-2">
-      Please provide your creator name to help keep your hashes unique.
-    </p>
-    <input
-      class="w-100"
-      type="text"
-      placeholder="Creator Name..."
-      bind:value={creatorName}
-    />
+    {#if page === "name"}
+      <p>
+        String Table Studio is a web app that makes it easy to create, edit, and
+        translate string tables for Sims 4 mods. View the <a
+          href="https://github.com/sims4toolkit/stbl-studio-web/README.md"
+          target="_blank">feature list</a
+        > to learn more about what it can do.
+      </p>
+      <p class="mb-2">
+        Please provide your creator name to help keep your hashes unique.
+      </p>
+      <input
+        class="w-100"
+        type="text"
+        placeholder="Creator Name..."
+        bind:value={creatorName}
+      />
+    {:else if page === "disclaimers"}
+      <p>Thanks, {creatorName}! Before proceeding, please keep in mind:</p>
+      <ul>
+        <li>
+          The data that this website uses is stored locally on your computer. If
+          you clear your browser's cookies, you will lose your data.
+        </li>
+        <li>
+          You can download your data at any time, and use it to backup, restore,
+          or move your entire workspace to another browser or device.
+        </li>
+        <li>
+          Your data is your responsibility. I am not at fault for any losses.
+        </li>
+      </ul>
+    {/if}
   </div>
   <div class="flex-space-between">
-    <ProgressCircles circles={2} filled={nameFilledIn ? 1 : 0} />
+    <ProgressCircles circles={2} filled={filledInCircles} />
     <NavigationButton
-      text="Next"
+      text={page === "name" ? "Next" : "Get Started"}
       direction="right"
       bind:active={nameFilledIn}
       onClick={nextButtonClicked}
     />
   </div>
-  <div class="upload-workspace text-center">
-    <span on:click={uploadWorkspaceButtonClicked}
-      >I have a workspace to upload.</span
-    >
-  </div>
+  {#if page !== "upload"}
+    <div class="upload-workspace text-center">
+      <span on:click={uploadWorkspaceButtonClicked}
+        >I have a workspace to upload.</span
+      >
+    </div>
+  {/if}
 </div>
 
 <style lang="scss">
@@ -94,6 +120,10 @@
         text-decoration: none;
         cursor: pointer;
       }
+    }
+
+    li {
+      margin-bottom: 0.35em;
     }
   }
 </style>
