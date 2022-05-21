b20316a13937af0b8410fe50ea97415deb7de994
Frank Kulak <fs.kulak@gmail.com>
2022-05-21 10:01:21 -0400

    Update toolbar-colors.ts

--- src/enums/toolbar-colors.ts
+++ src/enums/toolbar-colors.ts
@@ -1,10 +1,9 @@
-// HACK: This should be an enum, but svelte is being annoying about it
-const ToolbarColor = {
-  Download: "#3DA8D0",
-  Upload: "#6B5AC6",
-  Create: "#48AD6F",
-  Delete: "#D35454",
-  Merge: "#BC47B7",
+enum ToolbarColor {
+  Download = "#3DA8D0",
+  Upload = "#6B5AC6",
+  Create = "#48AD6F",
+  Delete = "#D35454",
+  Merge = "#BC47B7",
 }
 
 export default ToolbarColor;
