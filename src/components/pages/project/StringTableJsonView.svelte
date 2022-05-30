58eface7b31f297b940de92ec02b4e6a2a5329a9
Frank Kulak <fs.kulak@gmail.com>
2022-05-29 21:24:10 -0400

    Update StringTableJsonView.svelte

--- src/components/pages/project/StringTableJsonView.svelte
+++ src/components/pages/project/StringTableJsonView.svelte
@@ -1,20 +1,24 @@
 <script lang="ts">
   import type { StringTableResource } from "@s4tk/models";
   import { onMount } from "svelte";
-  import { parse } from "uuid";
+  import { validateHexString } from "../../../typescript/helpers/tgi";
   import Checkbox from "../../shared/elements/Checkbox.svelte";
   import IconTextButton from "../../shared/elements/IconTextButton.svelte";
+  import ScreenDimmer from "../../shared/layout/ScreenDimmer.svelte";
+
+  const { formatStringKey } = window.S4TK.formatting;
 
   export let stbl: StringTableResource;
 
   let textarea: HTMLTextAreaElement;
   let wrapText = true;
   let fullHeight = false;
+  let errorMsg = "";
 
   let jsonContent = JSON.stringify(
     stbl?.entries.map((entry) => {
       return {
-        key: entry.key,
+        key: formatStringKey(entry.key),
         value: entry.value,
       };
     }) ?? "Loading...",
@@ -24,30 +28,49 @@
 
   onMount(() => {
     textarea.setAttribute("style", "height:" + textarea.scrollHeight + "px;");
-
-    textarea.addEventListener("input", OnInput, false);
+    textarea.addEventListener("input", onInput, false);
   });
 
-  function OnInput() {
+  function onInput() {
     this.style.height = "auto";
     this.style.height = this.scrollHeight + "px";
   }
+
+  function saveJson() {
+    try {
+      const entries = JSON.parse(jsonContent).map(({ key, value }, i) => {
+        if (!validateHexString(key, 8))
+          throw new Error(
+            `Key "${key}" of entry ${i} is not a valid 32-bit hex string.`
+          );
+
+        return {
+          key: parseInt(key, 16), // FIXME: verify it's a uint32
+          value,
+        };
+      });
+
+      stbl.clear();
+      stbl.addAll(entries);
+    } catch (err) {
+      console.error(err);
+      errorMsg = err;
+    }
+  }
 </script>
 
-<div class="flex-space-between">
+<div class="flex-space-between mb-1">
   <div class="flex flex-gap">
     <Checkbox label="Wrap text" bind:checked={wrapText} />
     <Checkbox label="Full height" bind:checked={fullHeight} />
   </div>
-  <IconTextButton
-    icon="save-outline"
-    text="Save"
-    onClick={() => {
-      stbl.clear();
-      stbl.addAll(JSON.parse(jsonContent));
-    }}
-  />
+  <IconTextButton icon="save-outline" text="Save" onClick={saveJson} />
 </div>
+
+<p class="subtle-text mb-2">
+  <u class="error-color bold">WARNING</u>: JSON view does not autosave. You must
+  click the save button when you are finished.
+</p>
 
 <textarea
   bind:this={textarea}
@@ -56,6 +79,15 @@
   class:full-height={fullHeight}
   class:wrap-text={wrapText}
 />
+
+{#if errorMsg}
+  <ScreenDimmer>
+    <div class="flex-center h-100">
+      {errorMsg}
+      <button on:click={() => (errorMsg = "")}>OK</button>
+    </div>
+  </ScreenDimmer>
+{/if}
 
 <style lang="scss">
   textarea {
