42bb96e10386f57a901407313351b697e476c4a4
Frank Kulak <fs.kulak@gmail.com>
2022-06-26 11:47:09 -0400

    Update StringRehashView.svelte

--- src/components/pages/project/StringRehashView.svelte
+++ src/components/pages/project/StringRehashView.svelte
@@ -9,12 +9,17 @@
   import { onDestroy } from "svelte";
 
   const { fnv32 } = window.S4TK.hashing;
+  const { formatStringKey } = window.S4TK.formatting;
 
   export let project: Project;
   export let selectionGroup: SelectionGroup<StringEntry, number>;
   export let onComplete: () => void;
 
+  let currentPage = 1;
   let refreshMethod = 1; // string value
+  let replacements: [number, number][];
+  let changedReplacements: [number, number][];
+  let numUnchanged: number;
 
   const keySubscriptions = [subscribeToKey("Escape", onComplete)];
 
@@ -23,8 +28,8 @@
   });
 
   function handleNextButtonClick() {
-    const replacements: [number, number][] =
-      selectionGroup.allSelectedItems.map((entry) => {
+    if (currentPage === 1) {
+      replacements = selectionGroup.allSelectedItems.map((entry) => {
         const newKey =
           refreshMethod === 0
             ? fnv32(uuidv4())
@@ -33,44 +38,109 @@
         return [entry.key, newKey];
       });
 
-    project.updateKeys(replacements);
-    project = project;
-    selectionGroup.toggleSelectMode(false);
+      changedReplacements = replacements.filter(([old, next]) => old !== next);
+      numUnchanged = replacements.length - changedReplacements.length;
 
-    onComplete();
+      ++currentPage;
+    } else {
+      ++currentPage;
+
+      if (changedReplacements?.length) {
+        project.updateKeys(changedReplacements);
+        project = project;
+        selectionGroup.toggleSelectMode(false);
+      }
+
+      onComplete();
+    }
   }
 </script>
 
 <MultipageModalContent
   title="Rehash Keys"
-  numPages={1}
-  finalPageNextButtonText="Rehash Keys"
+  bind:currentPage
+  completePages={currentPage}
+  numPages={2}
+  finalPageNextButtonText={changedReplacements?.length ? "Rehash Keys" : "OK"}
   onNextButtonClick={handleNextButtonClick}
+  minimumContentHeight="200"
+  centerVertically={true}
 >
-  <div slot="content">
-    <Select
-      label="rehash method"
-      name="rehash-method-select"
-      bind:selected={refreshMethod}
-      options={[
-        {
-          value: 0,
-          text: "Random UUID",
-        },
-        {
-          value: 1,
-          text: "String Value",
-        },
-      ]}
-    />
-    <p class="subtle-text">
-      {#if refreshMethod === 0}Completely random UUIDs will be generated and
-        hashed for each string.{:else}The project's UUID concatenated with the
-        string's content will be hashed for each string.{/if}
-    </p>
+  <div slot="content" class="w-100">
+    <div class="w-100">
+      {#if currentPage === 1}
+        <Select
+          label="rehash method"
+          name="rehash-method-select"
+          bind:selected={refreshMethod}
+          options={[
+            {
+              value: 1,
+              text: "String Value",
+            },
+            {
+              value: 0,
+              text: "Random UUID",
+            },
+          ]}
+        />
+        <p class="subtle-text mb-0 mt-2">
+          {#if refreshMethod === 0}Completely random UUIDs will be generated and
+            hashed for each string.{:else}The project's UUID will be prefixed on
+            each string's value before hashing.{/if}
+        </p>
+      {:else if currentPage === 2}
+        {#if changedReplacements?.length === 0}
+          <p>
+            The chosen hashing method has no effect on any selected strings.
+          </p>
+        {:else}
+          {#if numUnchanged === 1}
+            <p class="mt-0">1 entry is unaffected by this change.</p>
+          {:else if numUnchanged > 1}
+            <p class="mt-0">
+              {numUnchanged} entries are unaffected by this change.
+            </p>
+          {/if}
+          <p class="mt-0">
+            Are you sure you want to rehash the following entries?
+          </p>
+          <ul class="rehash-summary">
+            {#each changedReplacements as changed, key (key)}
+              <li>
+                <div class="flex-center-v">
+                  <span class="monospace accent-color"
+                    >{formatStringKey(changed[0])}</span
+                  >
+                  &nbsp;&rarr;&nbsp;
+                  <span class="monospace accent-color"
+                    >{formatStringKey(changed[1])}</span
+                  >
+                  &nbsp;
+                  <span class="nowrap-truncate"
+                    >({project.primaryStbl.getByKey(changed[0]).value})</span
+                  >
+                </div>
+              </li>
+            {/each}
+          </ul>
+          <p class="subtle-text mb-0">
+            This action cannot be undone. After rehashing, you will have to
+            update the keys in your tuning.
+          </p>
+        {/if}
+      {:else}
+        <p>Replacing keys...</p>
+      {/if}
+    </div>
   </div>
 </MultipageModalContent>
 
 <style lang="scss">
-  // intentionally blank
+  ul.rehash-summary {
+    max-height: 80px;
+    width: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
 </style>
