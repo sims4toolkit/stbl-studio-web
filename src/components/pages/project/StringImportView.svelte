65292ab1e2f0305fdea277e899b6d651ca2991e6
Frank Kulak <fs.kulak@gmail.com>
2022-07-27 17:02:41 -0400

    Update StringImportView.svelte

--- src/components/pages/project/StringImportView.svelte
+++ src/components/pages/project/StringImportView.svelte
@@ -1,8 +1,49 @@
 <script lang="ts">
-  // intentionally blank
+  import FileInput from "../../shared/elements/FileInput.svelte";
+
+  import MultipageModalContent from "../../shared/layout/MultipageModalContent.svelte";
+
+  export let onComplete: () => void;
+
+  let currentPage = 1;
+  let uploadedFiles: FileList;
+  let filesInvalid = false;
+
+  function onNextButtonClick() {
+    if (currentPage === 1) {
+      currentPage++;
+    } else if (currentPage === 2) {
+      currentPage++;
+    } else {
+      onComplete();
+    }
+  }
 </script>
 
-<!-- TODO: -->
-<style lang="scss">
-  // intentionally blank
-</style>
+<MultipageModalContent
+  title="Import Strings"
+  canClickBack={false}
+  numPages={3}
+  completePages={currentPage}
+  bind:currentPage
+  finalPageNextButtonText="Import"
+  {onNextButtonClick}
+>
+  <div slot="content" class="w-100">
+    {#if currentPage === 1}
+      <FileInput
+        focusOnMount={true}
+        label="binaries, jsons, and/or packages only"
+        bind:files={uploadedFiles}
+        accept=".json,.stbl,.bnry,.binary,.package"
+        multiple={true}
+        bind:filesInvalid
+        errorMessage="No valid STBLs found"
+      />
+    {:else if currentPage === 2}
+      <p>page 2</p>
+    {:else}
+      <p>page 3</p>
+    {/if}
+  </div>
+</MultipageModalContent>
