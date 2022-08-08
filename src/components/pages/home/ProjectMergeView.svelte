7716f56d70a33686f8f6f94dbde00eace23cf66d
Frank Kulak <fs.kulak@gmail.com>
2022-08-08 15:50:15 -0400

    Update ProjectMergeView.svelte

--- src/components/pages/home/ProjectMergeView.svelte
+++ src/components/pages/home/ProjectMergeView.svelte
@@ -5,12 +5,15 @@
   import type Workspace from "../../../typescript/models/workspace";
   import { activeWorkspace } from "../../../typescript/stores";
   import GroupInstanceLocale from "../../shared/controls/GroupInstanceLocale.svelte";
+  import ProjectMetaDataPages from "../../shared/controls/ProjectMetaDataPages.svelte";
   import MultipageModalContent from "../../shared/layout/MultipageModalContent.svelte";
 
   const { formatAsHexString } = window.S4TK.formatting;
 
   export let selectedProjects: Project[];
   export let onComplete: () => void;
+
+  let completePages = 1;
 
   let groupHexString = formatAsHexString(selectedProjects[0].group, 8);
   let isGroupValid = true;
@@ -34,37 +37,53 @@
   });
 
   function mergeSelectedProjects() {
-    // workspace.deleteProjects(...selectedProjects);
-    onComplete();
+    if (completePages === 1) {
+      completePages++;
+    } else {
+      onComplete();
+    }
   }
 </script>
 
 <MultipageModalContent
   title="Confirm Merge"
-  numPages={1}
-  completePages={1}
-  currentPage={1}
+  numPages={2}
+  {completePages}
+  currentPage={completePages}
   finalPageNextButtonText="Merge Projects"
   onNextButtonClick={mergeSelectedProjects}
 >
   <div slot="content">
-    <p class="mt-2">Are you sure you want to merge the following projects?</p>
-    <ul class="mb-2 merge-summary">
-      {#each selectedProjects as project, key (key)}
-        <li>{project.name} ({project.numStrings} strings)</li>
-      {/each}
-    </ul>
-    <GroupInstanceLocale
-      {groupHexString}
-      {isGroupValid}
-      {instanceHexString}
-      {isInstanceValid}
-      {selectedLocale}
-    />
-    <p class="subtle-text my-2">
-      This cannot be undone. If you want to unmerge your projects, you'll have
-      to do it manually.
-    </p>
+    {#if completePages === 1}
+      <p class="mt-2">Are you sure you want to merge the following projects?</p>
+      <ul class="mb-2 merge-summary">
+        {#each selectedProjects as project, key (key)}
+          <li>{project.name} ({project.numStrings} strings)</li>
+        {/each}
+      </ul>
+      <p class="subtle-text my-2">
+        This cannot be undone. If you want to unmerge your projects, you'll have
+        to do it manually.
+      </p>
+    {:else if completePages === 2}
+      <GroupInstanceLocale
+        {groupHexString}
+        {isGroupValid}
+        {instanceHexString}
+        {isInstanceValid}
+        {selectedLocale}
+      />
+      <!-- <ProjectMetaDataPages
+        {uuid}
+        {currentPage}
+        bind:isPage1Valid
+        bind:primaryLocale
+        bind:name
+        bind:otherLocaleOptions
+        bind:groupHexString
+        bind:instanceHexString
+      /> -->
+    {/if}
   </div>
 </MultipageModalContent>
 
