8f55954ffd74f9ec48cfdb3af0df6b40caba6e9b
Frank Kulak <fs.kulak@gmail.com>
2022-08-10 14:57:49 -0400

    Update window.d.ts

--- src/window.d.ts
+++ src/window.d.ts
@@ -1,44 +1,22 @@
-import type { CompressionType, compressBuffer, decompressBuffer } from "@s4tk/compression";
-import type { BinaryEncoder, BinaryDecoder } from "@s4tk/encoding";
-import type { fnv32, fnv64 } from "@s4tk/hashing";
-import type { formatAsHexString, formatResourceKey, formatStringKey, formatResourceInstance } from "@s4tk/hashing/formatting";
-import type { Package, StringTableResource } from "@s4tk/models";
-import type { StringTableLocale, BinaryResourceType } from "@s4tk/models/enums";
-import type StringEntry from "@s4tk/models/lib/resources/stbl/string-entry";
+import type compression from "@s4tk/compression";
+import type encoding from "@s4tk/encoding";
+import type hashing from "@s4tk/hashing";
+import type formatting from "@s4tk/hashing/formatting";
+import type models from "@s4tk/models";
+import type enums from "@s4tk/models/enums";
 
 declare global {
   interface Window {
     S4TK: {
-      compression: {
-        CompressionType: typeof CompressionType;
-        compressBuffer: typeof compressBuffer;
-        decompressBuffer: typeof decompressBuffer;
-      },
-      encoding: {
-        BinaryEncoder: typeof BinaryEncoder;
-        BinaryDecoder: typeof BinaryDecoder;
-      },
-      enums: {
-        StringTableLocale: typeof StringTableLocale;
-        BinaryResourceType: typeof BinaryResourceType;
-      },
-      formatting: {
-        formatAsHexString: typeof formatAsHexString;
-        formatResourceKey: typeof formatResourceKey;
-        formatStringKey: typeof formatStringKey;
-        formatResourceInstance: typeof formatResourceInstance;
-      },
-      hashing: {
-        fnv64: typeof fnv64;
-        fnv32: typeof fnv32;
-      },
-      models: {
-        Package: typeof Package;
-        StringTableResource: typeof StringTableResource;
-      },
+      compression: typeof compression;
+      encoding: typeof encoding;
+      enums: typeof enums;
+      formatting: typeof formatting;
+      hashing: typeof hashing;
+      models: typeof models;
       Node: {
         Buffer: typeof Buffer;
-      }
+      };
     };
   }
 }
