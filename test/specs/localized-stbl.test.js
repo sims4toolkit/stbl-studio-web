58cac9eba0fb9c171e9d581b7568678593d0c3c7
Frank Kulak <fs.kulak@gmail.com>
2022-08-10 14:51:10 -0400

    Add skeleton for localized stbl tests.

--- test/specs/localized-stbl.test.js
+++ test/specs/localized-stbl.test.js
@@ -1,16 +1,80 @@
 const { expect } = chai;
 import LocalizedStringTable from "../lib/localized-stbl.js";
 
-describe("LocalizedStringTable", () => {
-  describe("#primaryLocale", () => {
-    it("should pass", () => {
-      const stbl = new LocalizedStringTable(0, new Set([0]));
-      expect(stbl.primaryLocale).to.equal(0);
-    });
+describe("LocalizedStringEntry", () => {
+  //#region Getters / Setters
 
-    it("should fail", () => {
-      const stbl = new LocalizedStringTable(0, new Set([0]));
-      expect(stbl.primaryLocale).to.equal(1);
-    });
+  describe("#allLocales", () => {
+    // TODO:
   });
+
+  describe("#entries", () => {
+    // TODO:
+  });
+
+  describe("#primaryLocale", () => {
+    // TODO:
+  });
+
+  //#endregion Getters / Setters
+
+  //#region Initialization
+
+  describe("#constructor", () => {
+    // TODO:
+  });
+
+  //#endregion Initialization
+
+  //#region Methods
+
+  describe("#addEntry()", () => {
+    // TODO:
+  });
+
+  describe("#deleteEntry()", () => {
+    // TODO:
+  });
+
+  describe("#getEntry()", () => {
+    // TODO:
+  });
+
+  describe("#getJson()", () => {
+    // TODO:
+  });
+
+  describe("#getStringTable()", () => {
+    // TODO:
+  });
+
+  describe("#getValue()", () => {
+    // TODO:
+  });
+
+  describe("#hasEntry()", () => {
+    // TODO:
+  });
+
+  describe("#importEntries()", () => {
+    // TODO:
+  });
+
+  describe("#replaceEntries()", () => {
+    // TODO:
+  });
+
+  describe("#replaceLocales()", () => {
+    // TODO:
+  });
+
+  describe("#setKey()", () => {
+    // TODO:
+  });
+
+  describe("#setString()", () => {
+    // TODO:
+  });
+
+  //#endregion Methods
 });
