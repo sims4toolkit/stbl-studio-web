e4829d9b0614070e119a1658903dd9d0977b5a35
Frank Kulak <fs.kulak@gmail.com>
2022-08-12 18:31:46 -0400

    Update project.test.js

---
+++ test/specs/models/project.test.js
@@ -0,0 +1,41 @@
+import MockStorage from "../../helpers/mock-storage.js";
+import StorageService from "../../lib/services/storage.js";
+import LocalizedStringTable from "../../lib/models/localized-stbl.js";
+import Project from "../../lib/models/project.js";
+const { expect } = chai;
+
+describe("Project", () => {
+  //#region Properties
+
+  // uuid & meta data tested as part of constructor & deserialize()
+
+  describe("#stbl", () => {
+    // TODO:
+  });
+
+  //#endregion Properties
+
+  //#region Initialization
+
+  describe("#constructor", () => {
+    // TODO:
+  });
+
+  describe("static#deserialize()", () => {
+    // TODO:
+  });
+
+  describe("static#deserializeMetaData()", () => {
+    // TODO:
+  });
+
+  //#endregion Initialization
+
+  //#region Methods
+
+  describe("#serializeMetaData()", () => {
+    // TODO:
+  });
+
+  //#endregion Methods
+});
