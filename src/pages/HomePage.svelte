29cb8b0bfffff13ffcff16ed89aa4d543a2ad294
Frank Kulak <fs.kulak@gmail.com>
2022-08-15 14:29:42 -0400

    Update grays.

--- src/pages/HomePage.svelte
+++ src/pages/HomePage.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 </script>
 
-<p>Home</p>
-
-<style lang="scss">
-  // intentionally blank
-</style>
+<div class="w-full flex justify-center">
+  <div class="w-full xl:max-w-screen-xl py-12 px-4">
+    <p>100</p>
+  </div>
+</div>
+<div class="w-full flex justify-center bg-gray-200 dark:bg-gray-700">
+  <div class="w-full xl:max-w-screen-xl py-12 px-4">
+    <p>200</p>
+  </div>
+</div>
