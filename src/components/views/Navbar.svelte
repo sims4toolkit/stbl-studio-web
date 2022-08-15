c5f9aaa18f4527d90fc137f177b25b3c607e806b
Frank Kulak <fs.kulak@gmail.com>
2022-08-15 12:17:08 -0400

    Add shadow class.

--- src/components/views/Navbar.svelte
+++ src/components/views/Navbar.svelte
@@ -1,8 +1,8 @@
-<div
-  class="fixed top-0 left-0 right-0 h-10 px-2 flex justify-between bg-gray-200 bg-opacity-90 dark:bg-gray-900 dark:bg-opacity-90"
+<nav
+  class="fixed top-0 left-0 right-0 h-10 px-2 flex justify-between bg-gray-200 dark:bg-gray-900 blurred-bg bottom-shadow"
 >
   <div class="flex gap-2 items-center">
-    <img src="./icon.png" alt="S4TK" class="h-5" />
+    <img src="./assets/s4tk-transparent.png" alt="S4TK" class="h-5" />
     <h2 class="font-bold">STBL Studio</h2>
   </div>
-</div>
+</nav>
