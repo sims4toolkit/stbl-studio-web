66088c775316b33de7894c57cce302d3c7553783
Frank Kulak <fs.kulak@gmail.com>
2022-08-15 23:08:29 -0400

    Touch up home action buttons.

--- src/components/controls/FloatingActionButtonTitle.svelte
+++ src/components/controls/FloatingActionButtonTitle.svelte
@@ -23,7 +23,7 @@
 
 <div
   bind:this={titleElement}
-  class="mb-2 rounded-md text-center text-sm uppercase py-1 text-white"
+  class="mb-2 text-center text-sm uppercase py-1 text-white"
   class:bg-gray-400={useDisabledColor}
   class:dark:bg-gray-600={useDisabledColor}
   transition:fly={{ y: 12, duration: Settings.reduceMotion ? 0 : 350 }}
@@ -33,6 +33,7 @@
 
 <style lang="scss">
   div {
+    border-radius: 4px;
     transition: background-color 200ms;
     transition-delay: 0ms;
   }
--- src/pages/home/views/HomeActionButtons.svelte
+++ src/pages/home/views/HomeActionButtons.svelte
@@ -4,34 +4,70 @@
   export let inModal: boolean;
   export let inSelectMode: boolean;
   export let numSelected: number;
+
+  /*
+    Save = "#305eba",
+    Download = "#3f9bcc",
+    Upload = "#6B5AC6",
+    Create = "#48AD6F",
+    Delete = "#D35454",
+    Merge = "#BC47B7",
+    Rehash = "#d17747",
+  */
+
+  $: buttonData = inSelectMode
+    ? [
+        {
+          color: "#3F9BCC",
+          title: "Download",
+          icon: "download",
+          onClick: () => {
+            console.log("click");
+          },
+        },
+        {
+          color: "#BC47B7",
+          title: "Merge",
+          icon: "git-merge",
+          onClick: () => {
+            console.log("click");
+          },
+        },
+        {
+          color: "#D35454",
+          title: "Delete",
+          icon: "trash",
+          onClick: () => {
+            console.log("click");
+          },
+        },
+      ]
+    : [
+        {
+          color: "#305EBA",
+          title: "Save Workspace",
+          icon: "desktop-download",
+          onClick: () => {
+            console.log("click");
+          },
+        },
+        {
+          color: "#6B5AC6",
+          title: "Upload Project",
+          icon: "upload",
+          onClick: () => {
+            console.log("click");
+          },
+        },
+        {
+          color: "#48AD6F",
+          title: "New Project",
+          icon: "plus",
+          onClick: () => {
+            console.log("click");
+          },
+        },
+      ];
 </script>
 
-<FloatingActionButtonGroup
-  buttonData={[
-    {
-      color: "#ff0000",
-      title: "First",
-      disabled: true,
-      icon: "plus",
-      onClick: () => {
-        console.log("click");
-      },
-    },
-    {
-      color: "#00ff00",
-      title: "Second",
-      icon: "download",
-      onClick: () => {
-        console.log("click");
-      },
-    },
-    {
-      color: "#0000ff",
-      title: "Third",
-      icon: "git-merge",
-      onClick: () => {
-        console.log("click");
-      },
-    },
-  ]}
-/>
+<FloatingActionButtonGroup {buttonData} />
