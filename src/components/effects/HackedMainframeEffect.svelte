d9ff7fee09caed61287cb0f3fa65755c87ef759f
Frank Kulak <fs.kulak@gmail.com>
2022-08-19 17:51:44 -0400

    Update HackedMainframeEffect.svelte

--- src/components/effects/HackedMainframeEffect.svelte
+++ src/components/effects/HackedMainframeEffect.svelte
@@ -7,22 +7,24 @@
 
   function createBinaryElement(): HTMLParagraphElement {
     const element = document.createElement("p");
-    element.classList.add("fixed", "monospace", "text-xs");
+    element.classList.add("fixed", "monospace");
     element.innerText = Math.random() < 0.5 ? "01" : "10";
     element.style.left = `${Math.random() * window.innerWidth}px`;
     element.style.top = "0";
+    element.style.fontSize = Math.round(10 + Math.random() * 8) + "px";
 
     let y = 0;
+    const increment = Math.round(10 + Math.random() * 15);
     let movingInterval = setInterval(() => {
       if (y > window.innerHeight) {
         clearInterval(movingInterval);
         movingIntervals.delete(movingInterval);
         hackedMainframeOverlay.removeChild(element);
       } else {
-        y += 12;
+        y += increment;
         element.style.top = y + "px";
       }
-    }, 350);
+    }, 85);
     movingIntervals.add(movingInterval);
 
     return element;
@@ -32,7 +34,7 @@
     spawningInterval = setInterval(() => {
       const element = createBinaryElement();
       hackedMainframeOverlay.appendChild(element);
-    }, 200);
+    }, 175);
   });
 
   onDestroy(() => {
