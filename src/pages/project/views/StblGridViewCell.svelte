da1f829a7184e8e5b9ef4bb08f914b1c7690da9f
Frank Kulak <fs.kulak@gmail.com>
2022-08-19 19:40:42 -0400

    Update StblGridViewCell.svelte

--- src/pages/project/views/StblGridViewCell.svelte
+++ src/pages/project/views/StblGridViewCell.svelte
@@ -10,20 +10,65 @@
 
   let keyValue = formatStringKey(entry.key);
   let stringValue = entry.values.get(0); // FIXME: figure out how to get primary locale
+
+  function toggleSelection() {
+    if (selectionGroup.selectMode) {
+      selectionGroup.toggleValue(entry);
+      selectionGroup = selectionGroup;
+    }
+  }
 </script>
 
-<div class="p-4 bg-gray-50 dark:bg-gray-700 rounded hacker-border-gray">
+<button
+  class="p-4 bg-gray-50 dark:bg-gray-700 rounded hacker-border-gray border border-gray-50 dark:border-gray-700 hacker-bg-black"
+  class:selected={selectionGroup.isSelected(entry)}
+  class:selectable={selectionGroup.selectMode}
+  tabindex={selectionGroup.selectMode ? 0 : -1}
+  on:click={toggleSelection}
+>
   <div class="w-full flex justify-between mb-4">
+    {#if selectionGroup.selectMode}
+      <div class="flex items-center">
+        <div
+          class="select-indicator rounded-full h-4 w-4 border border-solid border-gray-500 dark:border-gray-400"
+        />
+      </div>
+    {/if}
     <h4 class="text-sm text-primary monospace">{keyValue}</h4>
     {#if !selectionGroup.selectMode}
       <CopyStringEntryButtons key={keyValue} string={stringValue} />
     {/if}
   </div>
-
   <input
-    class="bg-gray-75 dark:bg-gray-675 rounded w-full px-2 py-1"
+    class="bg-gray-75 dark:bg-gray-675 rounded w-full px-2 py-1 placeholder:text-gray-400 dark:placeholder:text-gray-500"
     placeholder="Empty String"
     type="text"
     bind:value={stringValue}
+    tabindex={selectionGroup.selectMode ? -1 : 0}
   />
-</div>
+</button>
+
+<style lang="scss">
+  button {
+    cursor: default;
+
+    &.selectable {
+      cursor: pointer;
+      user-select: none;
+
+      input {
+        pointer-events: none;
+      }
+    }
+  }
+
+  .selected {
+    // !important only needed b/c hacker theme
+    border: 1px solid var(--color-accent-secondary) !important;
+
+    .select-indicator {
+      border-color: var(--color-accent-secondary);
+      background-color: var(--color-accent-secondary);
+    }
+  }
+</style>
