1f5bc17ae064add7c929b3fd671fdeee1d16abf2
Frank Kulak <fs.kulak@gmail.com>
2022-08-19 13:23:39 -0400

    Add project action buttons.

--- src/pages/project/ProjectPage.svelte
+++ src/pages/project/ProjectPage.svelte
@@ -3,15 +3,25 @@
   import { replace } from "svelte-spa-router";
   import type Project from "src/lib/models/project";
   import Workspace from "src/lib/models/workspace";
+  import SelectionGroup from "src/lib/models/selection-group";
+  import type { LocalizedStringEntry } from "src/lib/models/localized-stbl";
   import { activeWorkspaceStore } from "src/lib/services/stores";
   import Settings from "src/lib/services/settings";
   import ProjectPageHeader from "./views/ProjectPageHeader.svelte";
   import ViewSwitcher from "./controls/ViewSwitcher.svelte";
+  import ProjectActionButtons from "./controls/ProjectActionButtons.svelte";
 
   export let params: { uuid: string };
 
   let project: Project;
   let projectLoaded = false;
+  let selectionGroup = new SelectionGroup<LocalizedStringEntry, number>(
+    [],
+    "key",
+    () => {
+      console.log("test");
+    }
+  ); // FIXME:
 
   const subscriptions = [
     activeWorkspaceStore.subscribe(async (workspace) => {
@@ -49,6 +59,7 @@
         <ViewSwitcher bind:project />
       </div>
     </div>
+    <ProjectActionButtons bind:selectionGroup bind:project />
   {:else}
     <div class="w-full flex justify-center">
       <h1
---
+++ src/pages/project/controls/ProjectActionButtons.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+  import type Project from "src/lib/models/project";
+  import type SelectionGroup from "src/lib/models/selection-group";
+  import type { LocalizedStringEntry } from "src/lib/models/localized-stbl";
+  import type { FloatingActionButtonData } from "src/components/controls/types";
+  import FloatingActionButtonGroup from "src/components/controls/FloatingActionButtonGroup.svelte";
+  import BlurOverlay from "src/components/layouts/BlurOverlay.svelte";
+
+  export let selectionGroup: SelectionGroup<LocalizedStringEntry, number>;
+  export let project: Project;
+
+  const ifNotInModal = (fn: () => void) => {
+    return () => {
+      if (!inModal) fn();
+    };
+  };
+
+  let usingBlurOverlay = false;
+  let usingTransparentOverlay = false;
+  let modalContentComponent: any;
+  let modalContentArgs: object;
+  let buttonData: FloatingActionButtonData[];
+
+  $: inModal = usingBlurOverlay || usingTransparentOverlay;
+
+  $: {
+    buttonData = selectionGroup?.selectMode
+      ? [
+          {
+            color: "Orange",
+            title: "Rehash",
+            icon: "refresh",
+            keybinding: "r",
+            onClick: ifNotInModal(() => {
+              alert("rehash");
+            }),
+          },
+          {
+            color: "Red",
+            title: "Delete",
+            icon: "trash",
+            keybinding: "d",
+            onClick: ifNotInModal(() => {
+              alert("delete");
+            }),
+          },
+        ]
+      : [
+          {
+            color: "Azure",
+            title: "Download",
+            icon: "download",
+            keybinding: "s",
+            onClick: ifNotInModal(() => {
+              alert("download");
+            }),
+          },
+          {
+            color: "Purple",
+            title: "Import Strings",
+            icon: "upload",
+            keybinding: "u",
+            onClick: ifNotInModal(() => {
+              alert("upload");
+            }),
+          },
+          {
+            color: "Green",
+            title: "New String",
+            icon: "plus",
+            keybinding: "n",
+            onClick: ifNotInModal(() => {
+              alert("create");
+            }),
+          },
+        ];
+  }
+
+  function onModalClose() {
+    usingBlurOverlay = false;
+    usingTransparentOverlay = false;
+    modalContentComponent = undefined;
+    modalContentArgs = undefined;
+  }
+</script>
+
+<FloatingActionButtonGroup {buttonData} />
+
+{#if usingBlurOverlay}
+  <BlurOverlay onClose={onModalClose}>
+    <svelte:component
+      this={modalContentComponent}
+      onComplete={onModalClose}
+      bind:project
+      {...modalContentArgs}
+    />
+  </BlurOverlay>
+{:else if usingTransparentOverlay}
+  <p>TODO:</p>
+{/if}
