d8124f2526221acabdb9cae0278b0c16d072ad7e
Frank Kulak <fs.kulak@gmail.com>
2022-08-20 18:01:23 -0400

    Update StblGridViewCell.svelte

--- src/pages/project/views/StblGridViewCell.svelte
+++ src/pages/project/views/StblGridViewCell.svelte
@@ -4,6 +4,7 @@
   import type SelectionGroup from "src/lib/models/selection-group";
   import CopyStringEntryButtons from "src/components/controls/CopyStringEntryButtons.svelte";
   import ResizableTextArea from "src/components/elements/ResizableTextArea.svelte";
+  import { validateHexString } from "src/lib/utilities/tgi";
   const { formatStringKey } = window.S4TK.formatting;
 
   export let project: Project;
@@ -12,6 +13,10 @@
 
   let keyValue: string;
   let stringValue: string;
+  let keyInput: HTMLInputElement;
+
+  $: keyValueValid =
+    keyValue != undefined ? validateHexString(keyValue, 8) : true;
 
   $: {
     project;
@@ -27,6 +32,19 @@
     if (selectionGroup.selectMode) {
       selectionGroup.toggleValue(entry);
       selectionGroup = selectionGroup;
+    }
+  }
+
+  function onKeyInputBlur() {
+    keyValue = keyValue.trim();
+
+    if (keyValueValid) {
+      const newKey = parseInt(keyValue, 16);
+      project.stbl.setKey(entry.id, newKey);
+      project.stbl.saveToStorage(project.uuid);
+      project = project;
+    } else {
+      keyInput.focus();
     }
   }
 
@@ -46,32 +64,37 @@
   tabindex={selectionGroup.selectMode ? 0 : -1}
   on:click={toggleSelection}
 >
-  <div class="w-full flex justify-between mb-4">
-    {#if selectionGroup.selectMode}
-      <div class="flex items-center">
-        <div
-          class="select-indicator rounded-full h-4 w-4 border border-solid border-gray-500 dark:border-gray-400"
-        />
-      </div>
-    {/if}
-    <input
-      class="bg-transparent text-primary placeholder:text-gray-400 dark:placeholder:text-gray-500 monospace rounded p-1 text-sm w-24"
-      placeholder="0x00000000"
-      type="text"
-      bind:value={keyValue}
+  <div class="w-full">
+    <div class="w-full flex justify-between mb-4">
+      {#if selectionGroup.selectMode}
+        <div class="flex items-center">
+          <div
+            class="select-indicator rounded-full h-4 w-4 border border-solid border-gray-500 dark:border-gray-400"
+          />
+        </div>
+      {/if}
+      <input
+        bind:this={keyInput}
+        class="bg-transparent text-red-600 dark:text-red-500 placeholder:text-gray-400 dark:placeholder:text-gray-500 monospace rounded p-1 text-sm w-24"
+        class:valid={keyValueValid}
+        placeholder="0x00000000"
+        type="text"
+        bind:value={keyValue}
+        on:blur={onKeyInputBlur}
+        tabindex={selectionGroup.selectMode ? -1 : 0}
+      />
+      {#if !selectionGroup.selectMode}
+        <CopyStringEntryButtons key={keyValue} string={stringValue} />
+      {/if}
+    </div>
+    <ResizableTextArea
+      bind:value={stringValue}
+      placeholder="Empty string"
+      fillWidth={true}
       tabindex={selectionGroup.selectMode ? -1 : 0}
+      onBlur={saveString}
     />
-    {#if !selectionGroup.selectMode}
-      <CopyStringEntryButtons key={keyValue} string={stringValue} />
-    {/if}
   </div>
-  <ResizableTextArea
-    bind:value={stringValue}
-    placeholder="Empty string"
-    fillWidth={true}
-    tabindex={selectionGroup.selectMode ? -1 : 0}
-    onBlur={saveString}
-  />
 </button>
 
 <style lang="scss">
@@ -85,7 +108,15 @@
       input {
         pointer-events: none;
       }
+
+      & > div {
+        pointer-events: none;
+      }
     }
+  }
+
+  input.valid {
+    color: var(--color-accent);
   }
 
   .selected {
