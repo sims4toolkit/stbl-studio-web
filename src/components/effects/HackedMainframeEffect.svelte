3d0550e7bf8f2df1744b97b5d61182289156cbbe
Frank Kulak <fs.kulak@gmail.com>
2022-08-20 19:27:44 -0400

    Recycle elements in hacker effect overlay.

--- src/components/effects/HackedMainframeEffect.svelte
+++ src/components/effects/HackedMainframeEffect.svelte
@@ -3,25 +3,47 @@
 
   let spawningInterval: any;
   let hackedMainframeOverlay: HTMLDivElement;
+  const allNodes = new Set<HTMLParagraphElement>();
+  let recycledNodes: {
+    element: HTMLParagraphElement;
+    speed: number;
+  }[] = [];
 
   function createBinaryElement(seed: number) {
-    const element = document.createElement("p");
-    element.classList.add("fixed", "monospace");
-    element.innerText = (seed % 2).toString();
-    element.style.left = `${Math.random() * window.innerWidth}px`;
+    let element: HTMLParagraphElement;
+    let speed: number;
 
-    const speedAndSizeRandom = Math.random();
-    const speed = Math.round(800 + (1 - speedAndSizeRandom) * 2200);
-    element.style.fontSize = Math.round(6 + speedAndSizeRandom * 8) * 2 + "px";
-    element.style.transition = `top ${speed}ms`;
-    element.style.transitionTimingFunction = "linear";
+    if (recycledNodes.length > 0) {
+      const recycled = recycledNodes.shift();
+      element = recycled.element;
+      speed = recycled.speed;
+      element.style.left = `${Math.random() * window.innerWidth}px`;
+      element.style.transition = `top ${speed}ms`;
+      element.style.transitionTimingFunction = "linear";
+      element.hidden = false;
+    } else {
+      element = document.createElement("p");
+      allNodes.add(element);
+      element.classList.add("fixed", "monospace");
+      element.innerText = (seed % 2).toString();
+      element.style.left = `${Math.random() * window.innerWidth}px`;
+      const speedAndSizeRandom = Math.random();
+      speed = Math.round(800 + (1 - speedAndSizeRandom) * 2200);
+      element.style.fontSize =
+        Math.round(6 + speedAndSizeRandom * 8) * 2 + "px";
+      element.style.transition = `top ${speed}ms`;
+      element.style.transitionTimingFunction = "linear";
+      element.style.top = `-${element.style.fontSize}`;
+      hackedMainframeOverlay.appendChild(element);
+    }
 
-    element.style.top = "-20px";
-    hackedMainframeOverlay.appendChild(element);
     setTimeout(() => {
       element.style.top = window.innerHeight + "px";
       setTimeout(() => {
-        hackedMainframeOverlay?.removeChild(element);
+        element.hidden = true;
+        element.style.transition = "none";
+        element.style.top = `-${element.style.fontSize}`;
+        recycledNodes.push({ element, speed });
       }, speed + 50);
     }, 50);
   }
@@ -29,7 +51,7 @@
   onMount(() => {
     spawningInterval = setInterval(() => {
       for (let i = 0; i < 5; ++i) createBinaryElement(i);
-    }, 350);
+    }, 200);
   });
 
   onDestroy(() => {
