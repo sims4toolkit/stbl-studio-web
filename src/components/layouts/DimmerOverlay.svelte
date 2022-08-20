85284abe9e79afb6beacf3ac42e94ccd16131aee
Frank Kulak <fs.kulak@gmail.com>
2022-08-20 09:43:24 -0400

    Touch up new string modal.

--- src/components/layouts/DimmerOverlay.svelte
+++ src/components/layouts/DimmerOverlay.svelte
@@ -1,21 +1,72 @@
 <script lang="ts">
-  import { fade } from "svelte/transition";
   import { onMount, onDestroy } from "svelte";
+  import { fade } from "svelte/transition";
   import Settings from "src/lib/services/settings";
+  import { subscribeToKey } from "src/lib/utilities/keybindings";
+
+  export let onClose: () => void;
+
+  let overlay: HTMLDivElement;
+  let firstFocusableChild: Element;
+  let lastFocusableChild: Element;
 
   const bodyClassName = "overlay-active";
+  const focusQuery = "a, button, input, textarea, select";
+  const subscriptions = [subscribeToKey("Escape", onClose)];
 
   onMount(() => {
     document.body.classList.add(bodyClassName);
+    resetChildrenRefs();
+    window.addEventListener("focusin", onFocusIn);
+    window.addEventListener("focusout", onFocusOut);
   });
 
   onDestroy(() => {
     document.body.classList.remove(bodyClassName);
+    window.removeEventListener("focusin", onFocusIn);
+    window.removeEventListener("focusout", onFocusOut);
+    subscriptions.forEach((unsub) => unsub());
   });
+
+  function resetChildrenRefs() {
+    const focusableChildren = overlay.querySelectorAll(focusQuery);
+    firstFocusableChild = focusableChildren[0];
+    lastFocusableChild = focusableChildren[focusableChildren.length - 1];
+  }
+
+  function focusOnFirstChild() {
+    if (!document.contains(firstFocusableChild)) resetChildrenRefs();
+    //@ts-ignore I do know why it's being annoying about this one...
+    firstFocusableChild?.focus();
+  }
+
+  function focusOnLastChild() {
+    if (!document.contains(lastFocusableChild)) resetChildrenRefs();
+    //@ts-ignore I do know why it's being annoying about this one...
+    lastFocusableChild?.focus();
+  }
+
+  function onFocusOut(e: FocusEvent) {
+    //@ts-ignore Idk why TS is being annoying about this...
+    if (!overlay.contains(e.relatedTarget)) {
+      if (lastFocusableChild === e.target) {
+        focusOnFirstChild();
+      } else if (firstFocusableChild === e.target) {
+        focusOnLastChild();
+      }
+    }
+  }
+
+  function onFocusIn(e: FocusEvent) {
+    //@ts-ignore Idk why TS is being annoying about this...
+    if (!overlay.contains(e.target)) focusOnFirstChild();
+  }
 </script>
 
 <div
-  transition:fade={{ duration: Settings.reduceMotion ? 0 : 500 }}
+  bind:this={overlay}
+  in:fade={{ duration: Settings.reduceMotion ? 0 : 350 }}
+  out:fade={{ duration: Settings.reduceMotion ? 0 : 200 }}
   class="fixed top-0 right-0 bottom-0 left-0 z-40 bg-gray-400 bg-opacity-60 dark:bg-gray-900 dark:bg-opacity-60"
 >
   <slot />
--- src/pages/project/views/NewStringModal.svelte
+++ src/pages/project/views/NewStringModal.svelte
@@ -4,6 +4,7 @@
   import type Project from "src/lib/models/project";
   import Settings, { rickGifStore } from "src/lib/services/settings";
   import Switch from "src/components/elements/Switch.svelte";
+  import ResizableTextArea from "src/components/elements/ResizableTextArea.svelte";
 
   export let project: Project;
   export let onComplete: () => void;
@@ -39,9 +40,9 @@
   }
 </script>
 
-<div class="fixed left-0 right-0 bottom-0 pb-12 flex justify-center">
+<div class="fixed left-0 right-0 bottom-0 pb-20 flex justify-center">
   <div
-    class="flex flex-col gap-4 w-full rounded p-4 max-w-6xl mx-4 bg-gray-200 dark:bg-gray-700 hacker-bg-black"
+    class="flex flex-col gap-4 w-full rounded p-4 max-w-6xl mx-4 bg-gray-100 dark:bg-gray-800 hacker-bg-black"
     in:fly={{ y: 20, duration: Settings.reduceMotion ? 0 : 350 }}
   >
     <div class="w-full flex justify-between">
@@ -50,13 +51,27 @@
         <img class="svg h-4" src="./assets/x.svg" alt="X" />
       </button>
     </div>
-    <textarea
-      class="w-full bg-transparent rounded border p-2 border-gray-400 dark:border-gray-600 resize-none"
+    <ResizableTextArea
       placeholder="New string"
+      fillWidth={true}
+      focusOnMount={true}
       bind:value
     />
-    <div class="w-full flex justify-between items-center">
-      <p class="text-subtle text-xs">Something</p>
+    <div class="w-full flex flex-wrap gap-2 justify-between items-center">
+      <p class="text-subtle text-xs">
+        Press <mark
+          class="keystroke bg-gray-200 dark:bg-gray-700 hacker-bg-gray"
+          >enter</mark
+        >
+        to save. Use
+        <mark class="keystroke bg-gray-200 dark:bg-gray-700 hacker-bg-gray"
+          >shift</mark
+        >
+        +
+        <mark class="keystroke bg-gray-200 dark:bg-gray-700 hacker-bg-gray"
+          >enter</mark
+        > for newlines.
+      </p>
       <div class="max-w-min whitespace-nowrap">
         <Switch label="Prompt Until Cancel" bind:checked={promptUntilCancel} />
       </div>
