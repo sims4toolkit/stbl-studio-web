a4cacb1bc63d71cac6e4ac0dff2ade1941b81f1c
Frank Kulak <fs.kulak@gmail.com>
2022-08-29 23:26:28 -0400

    Update TokenAssistantWindow.svelte

--- src/components/windows/TokenAssistantWindow.svelte
+++ src/components/windows/TokenAssistantWindow.svelte
@@ -4,13 +4,52 @@
 
   export let onClose: () => void;
 
-  let value: string = "";
+  let tokenIndex = 0;
+  let maleValue = "";
+  let femaleValue = "";
+
+  $: maleFemalePreview = `{M${tokenIndex}.${maleValue}}{F${tokenIndex}.${femaleValue}}`;
+
+  function copy(string: string) {
+    // TODO:
+  }
 </script>
 
 <MovableWindow title="Token Assistant" {onClose}>
   <div class="flex flex-col gap-4">
+    <div class="flex justify-between items-center gap-8">
+      <p class="text-sm whitespace-nowrap max-w-min">Token Index:</p>
+      <input
+        type="number"
+        min="0"
+        bind:value={tokenIndex}
+        class="w-full block h-10 px-2 rounded text-sm placeholder-gray-500 dark:placeholder-gray-500 bg-transparent border"
+        placeholder="#"
+      />
+    </div>
     <div>
-      <p>TODO</p>
+      <p class="text-subtle text-xs font-bold uppercase mb-2">
+        Male/Female Pair
+      </p>
+      <div class="flex justify-between gap-2">
+        <TextInput
+          name="male-token-input"
+          placeholder="Male..."
+          bind:value={maleValue}
+          fillWidth={true}
+        />
+        <TextInput
+          name="female-token-input"
+          placeholder="Female..."
+          bind:value={femaleValue}
+          fillWidth={true}
+        />
+      </div>
+      <button
+        class="monospace hover:text-accent-primary-light dark:hover:text-accent-primary-dark text-left text-sm mt-2"
+        on:click={() => copy(maleFemalePreview)}>{maleFemalePreview}</button
+      >
+      <p class="text-xs text-subtle mt-1">Click to copy.</p>
     </div>
   </div>
 </MovableWindow>
