d9b88aa91a0ed5bd8065f67e1e88bb350d3c9998
Frank Kulak <fs.kulak@gmail.com>
2022-08-30 20:20:45 -0400

    Update help menu.

--- src/components/windows/HelpWindow.svelte
+++ src/components/windows/HelpWindow.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  // import Settings from "src/lib/services/settings";
+  import Settings from "src/lib/services/settings";
   import helpMenuData from "src/data/help-menu.json";
   import MovableWindow from "src/components/layouts/MovableWindow.svelte";
-  import HelpRoute from "../help/HelpRoute.svelte";
+  import HelpRoute from "src/components/help/HelpRoute.svelte";
 
   export let onClose: () => void;
   export let args: { route?: string } = {};
@@ -12,10 +12,19 @@
 </script>
 
 <MovableWindow title="Help" {onClose}>
-  <div class="flex flex-col gap-4">
-    <!-- <div>
-      <p>Found {Settings.foundEasterEggs.length} of 4 easter eggs.</p>
-    </div> -->
+  <div class="flex-1 flex flex-col gap-4">
     <HelpRoute {routeData} />
+    {#if route === "/faqs"}
+      <hr class="border-black dark:border-white hacker-border-gray" />
+      <div>
+        <h4 class="text-sm font-bold mb-2">
+          Does this website have easter eggs?
+        </h4>
+        <p class="text-subtle text-sm">
+          It was created by Frankk. Of course it does. You've found {Settings
+            .foundEasterEggs.length} of 4 easter eggs.
+        </p>
+      </div>
+    {/if}
   </div>
 </MovableWindow>
